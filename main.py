43c418e5f71db063afd07c71c11d01cd050ed023
chandram13 <mschandr@uci.edu>
2022-08-15 16:26:08 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Marvish Chandra
+
+
+def currentYear(make):
+    make = input("What make Toyota vehicle do you currently own or intend to own?")
+    if make == "Corolla":
+        wheelsParts = []
+        exteriorParts = []
+        interiorParts = []
+        lightingParts = []
+        steering_suspension_Parts = []
+        engineParts = []
+        air_fuel_Parts = []
+        transmissionParts = []
+        brakeParts = []
+    if make == "Camry":
+
